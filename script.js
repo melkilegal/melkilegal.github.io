1b231ccc688b0ccb51c77ed10344e7dceb856f2a
melkilegal <melkeyone@gmail.com>
2026-08-06 15:30:22 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+// Smooth scroll enhancement
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+
+        if (target) {
+            target.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
